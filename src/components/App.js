0fb3396a8a49aabfcbcc3bfd555a3a57692725eb
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-03-24 15:52:43 +0530

    fetchonmount

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+ const [dt,setDt]=useState([]);
 
+useEffect(()=>{
+ fetch("https://jsonplaceholder.typicode.com/posts")
+ .then((response)=>response.json())
+ .then((data)=>{
+  console.log(data);
+  setDt(data);
+ });
+ 
+},[dt])
+
+return(
+<div>
+  <ol style={{fontWeight:'bold'}}>
+  {dt.map((value)=>{
+    
+    return(
+      <div>
+          <li><h2>{value.title}</h2></li>
+          <p>{value.body}</p>
+          </div>
+    )
+  })}
+  </ol>
+  
+</div>)
+}
 export default App
+
